e2eb5791280da50fd98fb7f98fa249a015b6f68c
BUGPLUS\Administrator <bugplus@163.com>
2025-06-17 13:33:50 +0800

    -- test read&write imu init data；

--- source_code/imu_9_keil/user/imu_algorithm/imu_9.c
+++ source_code/imu_9_keil/user/imu_algorithm/imu_9.c
@@ -46,6 +46,7 @@ void read_flash_information(void)
 	STMFLASH_Read(MAG_OFFSET_XSF_ADDR,(uint8_t*)&xsf_read_buf,2);  //
 	STMFLASH_Read(MAG_OFFSET_YSF_ADDR,(uint8_t*)&ysf_read_buf,2);  //
 
+	usb_printf("xsf_read_buf=%d , ysf_read_buf=%d \r\n", xsf_read_buf, ysf_read_buf);
 	// todo flash数据为空，赋默认值，代码需要继续简化；
     if(imu_9.acc_zero[0]==-1) //默认0点是0
     {
